e557e7758130ed5257ebf5f68d021a9ec73cd4b3
lakshmanaram <lakshmanaram.n@gmail.com>
2016-09-06 21:04:59 +0530

    adds compression

---
+++ compress.m
@@ -0,0 +1,26 @@
+loadImage;
+X = [ I zeros(size(I,1) , 1)];
+Output = zeros(size(I,1),size(X,2)/3);
+types = zeros(1,8);
+for i = 1:size(X,1)
+for j = 1:3:size(X,2)
+[Output(i,((j-1)/3)+1), type] = getBit(X(i,[j:j+2]));
+types(1,type) += 1;
+endfor
+endfor
+sumtypes = sum(types);
+types = types./sumtypes;
+types = types.*100;
+types = int8(types);
+disp("Processing done");
+Output = logical(Output);
+disp("Converted to logical values");
+imwrite(Output,"compressedImg","bmp");
+disp("Output saved as compressedImg.bmp");
+disp("Program paused. Press enter to continue");
+pause;
+imshow(Output);
+disp("Showing image and compressed image together");
+disp("Program paused. Press enter to continue");
+pause;
+imshow([I,Output]);
--- loadImage.m
+++ loadImage.m
@@ -1,3 +1,8 @@
 pkg load image;
+disp("Loading Image library");
 I = imread("image.jpg");
 I = im2bw(I);
+disp("Image Read and converted to Black and white");
+disp("Program paused. Press enter to continue");
+pause;
+imshow(I);
